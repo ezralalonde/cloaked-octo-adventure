bb4d54cb3ffb69da2b112e94e3598795d0806e17
ezralalonde <ezra@usefuliftrue.com>
2013-04-17 00:06:24 -0600

    :Fmt PS: added auto-:Fmt to my vimrc so I don't have to put up with this any more.

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
+package main
 
+import (
+	"cloaked-octo-adventure/swap"
+	"fmt"
+)
+
+func main() {
+	var lines int
+	_, err := fmt.Scanf("%d", &lines)
+	if err != nil {
+		panic("could not read lines")
+	}
+	for ii := 0; ii < lines; ii++ {
+		var width int
+		var train []int
+
+		_, err := fmt.Scanf("%d", &width)
+		if err != nil {
+			fmt.Println(lines, width, err)
+			panic("could not read width")
+		}
+		for jj := 0; jj < width; jj++ {
+			var value int
+			_, err := fmt.Scanf("%d", &value)
+			if err != nil {
+				panic("could not read value")
+			}
+			train = append(train, value)
+		}
+		fmt.Printf("Optimal train swapping takes %v swaps.\n", swap.Swap(train))
+	}
+}
--- swap/swap.go
+++ swap/swap.go
@@ -1,13 +1,13 @@
 package swap
 
 func Swap(in []int) (out int) {
-    for ii := 0; ii < len(in); ii++ {
-        for jj := ii; jj < len(in); jj++ {
-            if in[ii] > in[jj] {
-                in[ii], in[jj] = in[jj], in[ii]
-                out++
-            }
-        }
-    }
+	for ii := 0; ii < len(in); ii++ {
+		for jj := ii; jj < len(in); jj++ {
+			if in[ii] > in[jj] {
+				in[ii], in[jj] = in[jj], in[ii]
+				out++
+			}
+		}
+	}
 	return
 }
